42dcb5fb0e00b6f43b8acd42fe5eee2911f178a3
donatien de gervillier <donatien.dg@outlook.com>
2022-04-20 10:57:12 +0200

    Win conditions

--- src/Game/BoardGameBox.java
+++ src/Game/BoardGameBox.java
@@ -7,41 +7,49 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Map;
+import java.util.Observable;
+import java.util.Observer;
 
-public class BoardGameBox extends JPanel implements MouseListener {
+public class BoardGameBox extends Observable implements MouseListener{
     private final Dimension dim;
     private Map<Pair, Boolean> tab;
+    private JPanel jPanel = new JPanel(true){
+        @Override
+        public void paintComponent(Graphics g){
+            g.setColor(Color.white);
+            if(jPanel.getSize().height > jPanel.getSize().width){
+                g.fillOval(5,5, jPanel.getSize().width - 10, jPanel.getSize().width - 10);
+            }else{
+                g.fillOval(5,5, jPanel.getSize().height - 10, jPanel.getSize().height - 10);
+            }
+        }
+    };
 
     public BoardGameBox(Dimension dim, Map<Pair, Boolean> tab){
         this.dim = dim;
         this.tab = tab;
-        addMouseListener(this);
-    }
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        g.setColor(Color.white);
-        if(getSize().height > getSize().width){
-            g.fillOval(5,5, getSize().width - 10, getSize().width - 10);
-        }else{
-            g.fillOval(5,5, getSize().height - 10, getSize().height - 10);
-        }
+        jPanel.addMouseListener(this);
     }
 
     public void drawCircle(Graphics g, Color color){
 
-        this.getGraphics().setColor(color);
+        jPanel.getGraphics().setColor(color);
         if(dim.height > dim.width){
-            this.getGraphics().fillOval(4,4, dim.width + 5, dim.width + 5);
+            jPanel.getGraphics().fillOval(4,4, dim.width + 5, dim.width + 5);
         }else{
-            this.getGraphics().fillOval(4,4, dim.height - 10, dim.height - 10);
+            jPanel.getGraphics().fillOval(4,4, dim.height - 10, dim.height - 10);
         }
+    }
+
+    public JPanel getjPanel(){
+        return jPanel;
     }
 
     @Override
     public void mouseClicked(MouseEvent e){
-        int x = (int) getLocation().getX() / getWidth();
-        int y = (int) getLocation().getY() / getHeight();
+        int x = (int) jPanel.getLocation().getX() / jPanel.getWidth();
+        int y = (int) jPanel.getLocation().getY() / jPanel.getHeight();
 
         if (!tab.get(new Pair(y+1, x))){
             System.out.println("ca pose pas");
@@ -51,7 +59,10 @@ public class BoardGameBox extends JPanel implements MouseListener {
             System.out.println("ca pose");
             tab.replace(location, true);
 
-            drawCircle(getGraphics(), new Color(0,0,255,0));
+            drawCircle(jPanel.getGraphics(), new Color(0,0,255,0));
+
+            setChanged();
+            notifyObservers();
         }
     }
 
--- src/Game/GridGame.java
+++ src/Game/GridGame.java
@@ -6,15 +6,22 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Observable;
+import java.util.Observer;
 
-public class GridGame extends JPanel{
+public class GridGame extends JPanel implements Observer {
+    private int rows;
+    private int col;
+    private Map<Pair, Boolean> tab;
 
-    public GridGame(int rows, int col){
+    public GridGame(int rows, int col) {
+        this.rows = rows;
+        this.col = col;
 
         Dimension dFrame = new Dimension(rows * 100, col * 100);
         Dimension dBox = new Dimension(100, 100);
 
-        Map<Pair, Boolean> tab = new HashMap<>();
+        tab = new HashMap<>();
 
         setLayout(new GridLayout(rows, col));
         setSize(dFrame);
@@ -24,7 +31,9 @@ public class GridGame extends JPanel{
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < col; j++) {
                 tab.put(new Pair(i, j), false);
-                add(new BoardGameBox(dBox, tab));
+                BoardGameBox boardGameBox = new BoardGameBox(dBox, tab);
+                add(boardGameBox.getjPanel());
+                boardGameBox.addObserver(this);
             }
         }
         for (int j = 0; j < col; j++) {
@@ -37,5 +46,42 @@ public class GridGame extends JPanel{
             System.out.println();
         }
         setVisible(true);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        System.out.println("Updating");
+        int buffer;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < col - 3; j++) {
+                if (tab.get(new Pair(i, j)) && tab.get(new Pair(i, j + 1)) && tab.get(new Pair(i, j + 2)) && tab.get(new Pair(i, j + 3))) {
+                    System.out.println("win line");
+                }
+            }
+        }
+
+        for (int i = 0; i < rows - 3; i++) {
+            for (int j = 0; j < col; j++) {
+                if (tab.get(new Pair(i, j)) && tab.get(new Pair(i + 1, j)) && tab.get(new Pair(i + 2, j)) && tab.get(new Pair(i + 3, j))) {
+                    System.out.println("win col");
+                }
+            }
+        }
+
+        for (int i = 0; i < rows - 3; i++) {
+            for (int j = 0; j < col - 3; j++) {
+                if (tab.get(new Pair(i, j)) && tab.get(new Pair(i + 1, j + 1)) && tab.get(new Pair(i + 2, j + 2)) && tab.get(new Pair(i + 3, j + 3))) {
+                    System.out.println("win diagonal 1");
+                }
+            }
+        }
+
+        for (int i = 3; i < rows; i++) {
+            for (int j = 0; j < col - 3; j++) {
+                if (tab.get(new Pair(i, j)) && tab.get(new Pair(i - 1, j + 1)) && tab.get(new Pair(i - 2, j + 2)) && tab.get(new Pair(i - 3, j + 3))) {
+                    System.out.println("win diagonal 2");
+                }
+            }
+        }
     }
 }
