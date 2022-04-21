1b09df06e2d914a4d3cd03a262447a0b193c22c2
ThibautLffg <thiblffg@icloud.com>
2022-04-21 15:10:22 +0200

    pre menu

--- src/Game/BoardGameBox.java
+++ src/Game/BoardGameBox.java
@@ -14,6 +14,12 @@ public class BoardGameBox extends JPanel implements MouseListener{
 
     private final Dimension dim;
     private final Map<Pair, DataGameBox> tab;
+    public Color whit = new Color(255,255,255);
+    public Color r = new Color(255,0,0);
+    public int x,y,d;
+    public BoardGameBox boardGameBox;
+    public Color j = new Color(255,255,0);
+    public Color base;
 
     public BoardGameBox(Dimension dim, Map<Pair, DataGameBox> tab){
         this.dim = dim;
@@ -24,7 +30,11 @@ public class BoardGameBox extends JPanel implements MouseListener{
 
     @Override
     public void paintComponent(Graphics g){
-        g.setColor(Color.white);
+        if (base == null){
+            base= whit;
+        }
+
+        g.setColor(whit);
         if(getSize().height > getSize().width){
             g.fillOval(5,5, getSize().width - 10, getSize().width - 10);
         }else{
@@ -37,6 +47,8 @@ public class BoardGameBox extends JPanel implements MouseListener{
         int x = (int) getLocation().getX() / getWidth();
         int y = (int) getLocation().getY() / getHeight();
 
+        paintComponent(getGraphics());
+
         if (!tab.get(new Pair(y+1, x)).isUsed()){
             System.out.println("ca pose pas");
         }else{
@@ -46,16 +58,17 @@ public class BoardGameBox extends JPanel implements MouseListener{
             tab.get(location).setUsed(true);
 
             tab.get(location).update(location);
+
             drawCircle(Color.blue, tab.get(location).getPlayer());
         }
     }
 
     public void drawCircle(Color color, int player){
-
         getGraphics().setColor(color);
         if(dim.height > dim.width){
             getGraphics().fillOval(4,4, dim.width + 5, dim.width + 5);
         }else{
+
             getGraphics().fillOval(4,4, dim.height - 10, dim.height - 10);
         }
         String str = String.valueOf(player);
