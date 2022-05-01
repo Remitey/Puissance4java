a7bfe11d0cf8daf3a7c204c0e73be5efe592b551
ThomasM1712 <thomas.moura@edu.ece.fr>
2022-05-01 11:53:00 +0200

    BAse de donnéé encore....

--- src/BDD.sql
+++ src/BDD.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : dim. 17 avr. 2022 à 13:57
+-- Généré le : dim. 01 mai 2022 à 11:51
 -- Version du serveur :  10.4.18-MariaDB
 -- Version de PHP : 8.0.3
 
@@ -33,6 +33,14 @@ CREATE TABLE `cpu` (
   `DEPTH` int(8) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Déchargement des données de la table `cpu`
+--
+
+INSERT INTO `cpu` (`ID`, `DESCRIPTION`, `DEPTH`) VALUES
+(4, 'Hard', 4),
+(6, 'ok', 8);
+
 -- --------------------------------------------------------
 
 --
@@ -41,9 +49,9 @@ CREATE TABLE `cpu` (
 
 CREATE TABLE `game` (
   `ID` int(20) NOT NULL,
-  `PLAYER_1_ID` int(10) DEFAULT NULL,
-  `PLAYER_2_ID` int(10) DEFAULT NULL,
-  `CPU_LEVEL_ID` int(5) NOT NULL,
+  `PLAYER_1` int(10) DEFAULT NULL,
+  `PLAYER_2` int(10) DEFAULT NULL,
+  `CPU` int(5) DEFAULT NULL,
   `GRID_SIZE_row` int(8) DEFAULT NULL,
   `GRID_SIZE_col` int(10) DEFAULT NULL,
   `RESULT` int(2) DEFAULT NULL,
@@ -58,12 +66,19 @@ CREATE TABLE `game` (
 --
 
 CREATE TABLE `player` (
-   id INT(5) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  USERNAME varchar(15) DEFAULT NULL,
-  EMAIL varchar(50) DEFAULT NULL,
-  PASSWORD varchar(20) DEFAULT NULL,
-  PERMISSION int(3) DEFAULT NULL
-);
+  `ID` int(10) NOT NULL,
+  `USERNAME` varchar(15) DEFAULT NULL,
+  `EMAIL` varchar(50) DEFAULT NULL,
+  `PASSWORD` varchar(50) DEFAULT NULL,
+  `PERMISSION` int(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `player`
+--
+
+INSERT INTO `player` (`ID`, `USERNAME`, `EMAIL`, `PASSWORD`, `PERMISSION`) VALUES
+(1, 'oakajf', 'lad@yahhoo.fr', '123456', 2);
 
 -- --------------------------------------------------------
 
@@ -94,9 +109,9 @@ ALTER TABLE `cpu`
 --
 ALTER TABLE `game`
   ADD PRIMARY KEY (`ID`),
-  ADD KEY `Fk_Game_player1` (`PLAYER_1_ID`),
-  ADD KEY `Fk_Game_player2` (`PLAYER_2_ID`),
-  ADD KEY `Fk_Game_cpu` (`CPU_LEVEL_ID`);
+  ADD KEY `Fk_Game_player1` (`PLAYER_1`),
+  ADD KEY `Fk_Game_player2` (`PLAYER_2`),
+  ADD KEY `Fk_Game_cpu` (`CPU`);
 
 --
 -- Index pour la table `player`
@@ -119,9 +134,9 @@ ALTER TABLE `turn`
 -- Contraintes pour la table `game`
 --
 ALTER TABLE `game`
-  ADD CONSTRAINT `Fk_Game_cpu` FOREIGN KEY (`CPU_LEVEL_ID`) REFERENCES `cpu` (`ID`),
-  ADD CONSTRAINT `Fk_Game_player1` FOREIGN KEY (`PLAYER_1_ID`) REFERENCES `player` (`ID`),
-  ADD CONSTRAINT `Fk_Game_player2` FOREIGN KEY (`PLAYER_2_ID`) REFERENCES `player` (`ID`);
+  ADD CONSTRAINT `Fk_Game_cpu` FOREIGN KEY (`CPU`) REFERENCES `cpu` (`ID`),
+  ADD CONSTRAINT `Fk_Game_player1` FOREIGN KEY (`PLAYER_1`) REFERENCES `player` (`ID`),
+  ADD CONSTRAINT `Fk_Game_player2` FOREIGN KEY (`PLAYER_2`) REFERENCES `player` (`ID`);
 
 --
 -- Contraintes pour la table `turn`
